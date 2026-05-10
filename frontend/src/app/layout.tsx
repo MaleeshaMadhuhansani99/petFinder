93316936dad91880830c9ef5067b58e8a0750b13
MaleeshaMadhuhansani99 <maleeshamapalagama@gmail.com>
2026-05-10 09:20:58 +0530

    update login pages

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,9 @@
 import ReduxProvider from "@/components/ReduxProvider";
 import './globals.css'
 
-
 export const metaData = {
   title : 'Pet Finder',
   description : 'Find your perfect pet companion'
-
 }
 
 export default function RootLayout ({children}:{ children: React.ReactNode }){
